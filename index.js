4d18c8f0016d2ccf88a433c11c72607b742b8139
MariiaMarchenko <96719405+MariiaMarchenko@users.noreply.github.com>
2023-07-21 17:09:40 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function scrollToContact() {
+    const contact = document.getElementById("contact");
+    contact.scrollIntoView({ behavior: "smooth" });
+}
+function scrollToService() {
+    const contact = document.getElementById("service");
+    contact.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToProj() {
+    const contact = document.getElementById("gallery");
+    contact.scrollIntoView({ behavior: "smooth" });
+}
